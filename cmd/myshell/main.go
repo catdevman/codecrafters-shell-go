8bea5129535f6996d58c713d5aa8a4ddf341097a
Lucas Pearson <catdevman@gmail.com>
2024-06-17 20:37:43 -0400

    got it

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"bufio"
-	// Uncomment this block to pass the first stage
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	allowedCommands := []string{"exit"}
 	for {
 		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		fullCmd := strings.TrimRight(input, "\n")
+		cmdPieces := strings.Split(fullCmd, " ")
+		cmd := cmdPieces[0]
 
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		if slices.Contains(allowedCommands, cmd) {
+
+			if cmd == "exit" {
+				i, _ := strconv.ParseInt(cmdPieces[1], 32, 10)
+				os.Exit(int(i))
+			}
+		} else {
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+		}
 	}
 }
