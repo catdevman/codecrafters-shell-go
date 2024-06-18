1c77cffbc8aa5af1e9cdab0798ae15b3090e131e
Lucas Pearson <catdevman@gmail.com>
2024-06-17 20:06:32 -0400

    we'll see if this is what they're looking for

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,9 +8,19 @@ import (
 )
 
 func main() {
+	allowedCommands := []string{}
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	found := false
+	for _, cmd := range allowedCommands {
+		if cmd == command {
+			found = true
+		}
+	}
+	if !found {
+		fmt.Sprintf("%s: command not found", command)
+	}
 }
